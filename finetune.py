78257d014178a2c71f35c7ebb5ede45ce147fa3c
昊 吴 <wuhao@foda.ai>
2023-08-22 13:44:09 +0800

    Initial commit

---
+++ finetune.py
@@ -0,0 +1,112 @@
+import numpy as np
+import torch
+import transformers
+from datasets import load_dataset
+from transformers import AutoTokenizer, AutoModel
+import os
+from peft import PeftModel
+from peft import get_peft_model,LoraConfig,TaskType
+from transformers import Trainer, TrainingArguments, DataCollatorForSeq2Seq
+os.environ["WANDB_DISABLED"] = 'true'
+class Lora_finetune:
+    def __init__(self,last_name,timestamp):
+        self.last_name = last_name
+        self.timestamp = timestamp
+        self.tokenizer = AutoTokenizer.from_pretrained(self.last_name, trust_remote_code=True)
+        self.model = AutoModel.from_pretrained(self.last_name, trust_remote_code=True).cuda()
+        self.max_source_length = 512
+        self.max_target_length = 512
+        self.source_prefix = ''
+
+    # history处理
+    def preprocess(self,examples):
+        max_seq_length = self.max_source_length + self.max_target_length
+        model_inputs = {
+            "input_ids": [],
+            "labels": [],
+        }
+        for i in range(len(examples['key'])):
+            if examples['key'][i] and examples['value'][i]:
+                query, answer = examples['key'][i], examples['value'][i]
+
+                history = examples['history'][i]
+                prompt = self.tokenizer.build_prompt(query, history)
+
+                prompt = prompt
+                a_ids = self.tokenizer.encode(text=prompt, add_special_tokens=True, truncation=True,
+                                        max_length=self.max_source_length)
+                b_ids = self.tokenizer.encode(text=answer, add_special_tokens=False, truncation=True,
+                                        max_length=self.max_target_length)
+
+                context_length = len(a_ids)
+                input_ids = a_ids + b_ids + [self.tokenizer.eos_token_id]
+                labels = [self.tokenizer.pad_token_id] * context_length + b_ids + [self.tokenizer.eos_token_id]
+
+                pad_len = max_seq_length - len(input_ids)
+                input_ids = input_ids + [self.tokenizer.pad_token_id] * pad_len
+                labels = labels + [self.tokenizer.pad_token_id] * pad_len
+                labels = [(l if l != self.tokenizer.pad_token_id else -100) for l in labels]
+                model_inputs["input_ids"].append(input_ids)
+                model_inputs["labels"].append(labels)
+        return model_inputs
+
+    def train(self):
+        print("lora start")
+        ds_train_raw = load_dataset("json",data_files=os.path.join('datas',self.timestamp+'.json'))['train']
+
+
+        ds_train = ds_train_raw.map(
+            self.preprocess,
+            batched = True,
+            remove_columns=ds_train_raw.column_names
+        )
+
+        peft_config = LoraConfig(
+            task_type=TaskType.CAUSAL_LM,
+            inference_mode=False,
+            r=8,
+            lora_alpha=32,
+            lora_dropout=0.1,
+            target_modules=["key","value"]
+        )
+
+        peft_model = get_peft_model(self.model,peft_config)
+        
+        trainer = Trainer(
+            model=peft_model,
+            train_dataset=ds_train,
+            args=TrainingArguments(
+                per_device_eval_batch_size=1,
+                gradient_accumulation_steps=4,
+                warmup_steps=5,
+                max_steps=50,
+                learning_rate=5e-5,
+                logging_steps=1,
+                output_dir='outputs'
+            ),
+            data_collator=DataCollatorForSeq2Seq(
+                self.tokenizer
+            )
+        )
+
+        #训练时节约GPU占用
+        self.model.config.use_cache = True
+        self.model.supports_gradient_checkpointing = True 
+        self.model.gradient_checkpointing_enable()
+        self.model.enable_input_require_grads()
+        trainer.train()
+        ckpt_path = self.timestamp
+        peft_model.save_pretrained(ckpt_path)
+        model_old = AutoModel.from_pretrained(self.last_name,trust_remote_code=True)
+        peft_loaded = PeftModel.from_pretrained(model_old,ckpt_path)
+        model_new = peft_loaded.merge_and_unload()
+
+
+        save_path = 'chatglm2-update'
+        model_new.save_pretrained(save_path,max_shard_size = '2GB')
+        self.tokenizer.save_pretrained(save_path)
+        print('lora end')
+
+# if __name__ == '__main__':
+#     lora = Lora_finetune('THUDM/chatglm2-6b','2023-08-21-20-15-11')
+#     lora.train()
---
+++ infer.py
@@ -0,0 +1,215 @@
+import numpy as np
+import torch
+import transformers
+from transformers import AutoTokenizer, AutoModel
+from argparse import ArgumentParser
+import gradio as gr
+import mdtex2html
+import os
+from datetime import datetime
+import json
+import multiprocessing
+from finetune import Lora_finetune
+
+
+parser = ArgumentParser()
+
+parser.add_argument('--init',type=str,default="THUDM/chatglm2-6b")
+parser.add_argument('--last',type=str,default="chatglm2-update")
+parser.add_argument('--max_lifes',type=int,default=3)
+args = parser.parse_args()
+
+lifes = 0
+dialogs = []
+flag = 0
+tokenizer = AutoTokenizer.from_pretrained(args.init, trust_remote_code=True)
+model = AutoModel.from_pretrained(args.init, trust_remote_code=True).cuda()
+
+model = model.eval()
+
+#some code copy from https://github.com/THUDM/ChatGLM-6B
+
+def postprocess(self, y):
+    if y is None:
+        return []
+    for i, (message, response) in enumerate(y):
+        y[i] = (
+            None if message is None else mdtex2html.convert((message)),
+            None if response is None else mdtex2html.convert(response),
+        )
+    return y
+
+
+gr.Chatbot.postprocess = postprocess
+
+
+def parse_text(text):
+    """copy from https://github.com/GaiZhenbiao/ChuanhuChatGPT/"""
+    lines = text.split("\n")
+    lines = [line for line in lines if line != ""]
+    count = 0
+    for i, line in enumerate(lines):
+        if "```" in line:
+            count += 1
+            items = line.split('`')
+            if count % 2 == 1:
+                lines[i] = f'<pre><code class="language-{items[-1]}">'
+            else:
+                lines[i] = f'<br></code></pre>'
+        else:
+            if i > 0:
+                if count % 2 == 1:
+                    line = line.replace("`", "\`")
+                    line = line.replace("<", "&lt;")
+                    line = line.replace(">", "&gt;")
+                    line = line.replace(" ", "&nbsp;")
+                    line = line.replace("*", "&ast;")
+                    line = line.replace("_", "&lowbar;")
+                    line = line.replace("-", "&#45;")
+                    line = line.replace(".", "&#46;")
+                    line = line.replace("!", "&#33;")
+                    line = line.replace("(", "&#40;")
+                    line = line.replace(")", "&#41;")
+                    line = line.replace("$", "&#36;")
+                lines[i] = "<br>"+line
+    text = "".join(lines)
+    return text
+
+def save(input,response):
+    global lifes
+    global dialogs
+    global model
+    global tokenizer
+    global flag
+    lifes += 1
+    dialogs.append((input,response))
+    if lifes == args.max_lifes + 1:
+        dialogs = dialogs[:-1]
+        time = make_dataset(dialogs)
+        if not flag:
+            lora = Lora_finetune(args.init,time)
+        else:
+            lora = Lora_finetune(args.last,time)
+        flag += 1
+        sub_process = multiprocessing.Process(target=lora.train())
+        sub_process.start()
+        sub_process.join()
+        lifes = 0
+        dialogs = []
+        tokenizer = AutoTokenizer.from_pretrained(args.last, trust_remote_code=True)
+        model = AutoModel.from_pretrained(args.last, trust_remote_code=True).cuda()
+        model.eval()
+        
+
+def get_time():
+    current_datetime = datetime.now()
+
+    date_format = "%Y-%m-%d-%H-%M-%S"  # 指定日期格式
+    formatted_datetime = current_datetime.strftime(date_format)
+
+    return formatted_datetime
+
+def make_dataset(dialogs):
+    time = get_time()
+    with open(os.path.join('datas',time+'.json'),'w',encoding='utf-8') as f:
+        data_list = []
+        new_history = []
+        for i,(input,response) in enumerate(dialogs):
+            data_dict = {}
+            data_dict['key'] = input
+            data_dict['value'] = response
+            data_dict['history'] = new_history.copy()
+            new_history.append((data_dict['key'],data_dict['value']))
+            data_list.append(data_dict)
+        print(data_list)
+        f.write(json.dumps(data_list,ensure_ascii=False))
+        return time
+
+def make_dataset_SWAP(dialogs):
+    time = get_time()
+    with open(os.path.join('datas',time+'.json'),'w',encoding='utf-8') as f:
+        data_list = []
+        new_history = []
+        for i,(input,response) in enumerate(dialogs):
+            data_dict = {}
+            if i == 0:
+                data_dict['key'] = "AI正在等待用户提问。"
+                data_dict['value'] = "用户："+ input
+                data_dict['history'] = []
+                new_history.append((data_dict['key'],data_dict['value']))
+                data_list.append(data_dict)
+                temp_response = response
+            elif i != len(dialogs)-1:
+                data_dict['key'] = "AI："+temp_response
+                data_dict['value'] = "用户："+input
+                data_dict['history'] = new_history.copy()
+                new_history.append((data_dict['key'],data_dict['value']))
+                data_list.append(data_dict)
+                temp_response = response
+            else:
+                data_dict['key'] = "AI："+temp_response
+                data_dict['value'] = "用户："+input
+                data_dict['history'] = new_history.copy()
+                new_history.append((data_dict['key'],data_dict['value']))
+                data_list.append(data_dict)
+                data_dict_final = {}
+                data_dict_final['key'] = "AI："+response
+                data_dict_final['value'] = "对话已终止。"
+                data_dict_final['history'] = new_history.copy()
+                data_list.append(data_dict_final)
+        print(data_list)
+        f.write(json.dumps(data_list,ensure_ascii=False))
+        return time
+
+def predict(input, chatbot, max_length, top_p, temperature, history):
+    global lifes
+    global model
+    global tokenizer
+    chatbot.append((parse_text(input), ""))
+    if lifes >= args.max_lifes:
+        chatbot[-1] = (parse_text(input), parse_text('我困了，想要休息一会。'))
+        history = []
+        yield chatbot, history
+        save(input,'我困了，想要休息一会。')
+    else:
+        print(model)
+        for response, history in model.stream_chat(tokenizer, input, history, max_length=max_length, top_p=top_p,
+                                                temperature=temperature):
+            chatbot[-1] = (parse_text(input), parse_text(response))       
+            yield chatbot, history
+        save(input,response)
+    
+def reset_user_input():
+    return gr.update(value='')
+
+
+def reset_state():
+    return [], []
+
+
+with gr.Blocks() as demo:
+    gr.HTML("""<h1 align="center">LongLife ChatGLM</h1>""")
+
+    chatbot = gr.Chatbot()
+    with gr.Row():
+        with gr.Column(scale=4):
+            with gr.Column(scale=12):
+                user_input = gr.Textbox(show_label=False, placeholder="Input...", lines=10).style(
+                    container=False)
+            with gr.Column(min_width=32, scale=1):
+                submitBtn = gr.Button("Submit", variant="primary")
+        with gr.Column(scale=1):
+            emptyBtn = gr.Button("Clear History")
+            max_length = gr.Slider(0, 4096, value=2048, step=1.0, label="Maximum length", interactive=True)
+            top_p = gr.Slider(0, 1, value=0.7, step=0.01, label="Top P", interactive=True)
+            temperature = gr.Slider(0, 1, value=0.95, step=0.01, label="Temperature", interactive=True)
+
+    history = gr.State([])
+
+    submitBtn.click(predict, [user_input, chatbot, max_length, top_p, temperature, history], [chatbot, history],
+                    show_progress=True)
+    submitBtn.click(reset_user_input, [], [user_input])
+
+    emptyBtn.click(reset_state, outputs=[chatbot, history], show_progress=True)
+
+demo.queue().launch(share=True, inbrowser=True)
